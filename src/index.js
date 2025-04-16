6e28bd8d2f3a2085c0c9bd86ebf4980b61318389
dung <quangdung21102005@gmail.com>
2025-04-16 13:37:40 +0700

    change something to see cache-hit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello");
+    res.send("Hello world");
 })
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
